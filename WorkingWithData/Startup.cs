8d9b4c6929e7d49c44f2abb76305983a88e72b7f
Ivan Kyuchukov <i.kyuchukov@edynamix.com>
2020-06-13 00:58:23 +0300

    added AppInsights

--- WorkingWithData/Startup.cs
+++ WorkingWithData/Startup.cs
@@ -24,6 +24,9 @@ namespace WorkingWithData
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // The following line enables Application Insights telemetry collection.
+            services.AddApplicationInsightsTelemetry();
+
             services.AddControllersWithViews();
             services.AddTransient<ICandidateTypeService, CandidateTypeService>();
         }
